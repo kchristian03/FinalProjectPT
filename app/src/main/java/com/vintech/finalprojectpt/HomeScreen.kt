f0a0434b0075fb456db86d8f81ec883fbb620f15
Kevin @uc-imt <kchristian03@student.ciputra.ac.id>
2022-06-09 12:02:38 +0700

    binding

--- app/src/main/java/com/vintech/finalprojectpt/HomeScreen.kt
+++ app/src/main/java/com/vintech/finalprojectpt/HomeScreen.kt
@@ -2,10 +2,16 @@ package com.vintech.finalprojectpt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vintech.finalprojectpt.databinding.ActivityHomeScreenBinding
 
 class HomeScreen : AppCompatActivity() {
+    private lateinit var bind : ActivityHomeScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        bind = ActivityHomeScreenBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        setContentView(bind.root)
+
+
     }
 }
--- app/src/main/java/com/vintech/finalprojectpt/MainActivity.kt
+++ app/src/main/java/com/vintech/finalprojectpt/MainActivity.kt
@@ -2,10 +2,14 @@ package com.vintech.finalprojectpt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vintech.finalprojectpt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bind : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        bind = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(bind.root)
     }
 }
